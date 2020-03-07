186318e13fc5c2086ea2d4f4b360d9e5ff9c15dd
Emmanuel Ochoa <eorivera95@gmail.com>
2020-03-07 12:41:11 -0700

    Tratando de hacer funcionar slider(TIENE ERRORES)

--- src/components/Tratamientos/Tratamientos.js
+++ src/components/Tratamientos/Tratamientos.js
@@ -1,41 +1,93 @@
 import React from 'react';
 import Tarjeta from './Tarjeta';
-import data from '../../data/tratamientos.js';
+//import data from '../../data/tratamientos.js';
 class Tratamientos extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            properties:data,
-            property:data[2]
-        };
+            startPoint:0,
+            endPoint:0,
+            move:0,
+            sliderWidth:0
+        }
+        this.slider = React.createRef();
+        this.touchMove = this.touchMove.bind(this);
+        this.touchStart = this.touchStart.bind(this);
+        this.touchEnd = this.touchEnd.bind(this);
+        this.sliderMove = this.sliderMove.bind(this);
     }
-    nextProperty = ()=>{
+    
+    touchMove = (e)=>{
         
+        const moveX = e.touches[0].clientX;
         this.setState((state)=>({
-            property:data[state.property.id + 1]
+            move:moveX,
+            sliderWidth:this.slider.current.offsetWidth
         }));
     }
-    prevProperty = ()=>{
+    touchStart = (e)=>{
+        const startX = e.touches[0].clientX;
         this.setState((state)=>({
-            property:data[state.property.id - 1]
+            startPoint:startX
         }));
+    }
+    touchEnd = (e)=>{
+        const endX = e.changedTouches[0].pageX;
+        this.setState((state)=>({
+            endPoint:endX
+        }));
+    }
+    sliderMove(move,startPoint,sliderWidth,windowWidth){
+        let movementX,porcentageWindowX,porcentageSliderX;    
+        
+        movementX=move-startPoint;
+        porcentageWindowX=(movementX*100)/windowWidth;
+    
+        porcentageSliderX=(porcentageWindowX*sliderWidth)/windowWidth;
+        if(porcentageSliderX < (100/6)*-5){
+            porcentageSliderX =(100/6)*-5;
+        }else if(porcentageSliderX > 0){
+            porcentageSliderX= 0;
+        }
+        return porcentageSliderX;
     }
     render(){
-        const {properties, property} = this.state;
-        console.log("hola mundo");
+        const {properties, property} = this.props.estado;
+        const source = 'tratamientos';
+        /*console.log("hola mundo");
         console.log(property);
-        console.log(data.length);
         console.log(property.id*(100/properties.length));
+        console.log(properties);
+        console.log(property);*/
+        let {startPoint,move,endPoint,sliderWidth}= this.state;
+        let windowWidth = window.innerWidth;
+        console.log("SLIDER"+sliderWidth);
+        startPoint=parseFloat(startPoint);
+        move=parseFloat(move);
+        endPoint=parseFloat(endPoint);
+        
+        console.log(">>>>>>>>>>>>>>>>>>>>start"+startPoint);
+        console.log("move"+move);
+        console.log("end"+endPoint);
+        let slide
+        if(startPoint!=endPoint ){
+            slide=this.sliderMove(move,startPoint,sliderWidth,windowWidth)
+        }
+
+        
+        //`translateX(-${property.id*(100/properties.length)}%)`
+        
+        
         return(
             <>
             <button 
-            onClick={()=>this.nextProperty()}
-            disabled={property.id === (data.length-1)}
+            onClick={()=>this.props.nextProperty(source)}
+            disabled={property.id === (properties.length-1)}
             >
                 Siguiente
             </button>
             <button 
-            onClick={()=>this.prevProperty()}
+            onClick={()=>this.props.prevProperty(source)}
             disabled={property.id === 0}
             >
                 Anterior
@@ -45,10 +97,10 @@ class Tratamientos extends React.Component{
             <h2>Nuestros Tratamientos</h2>
             <p>Conoce todo sobre los tratamientos que DentalGM ofrece para ti</p>
             <section className="tratamientos__tarjetas">
-                <div className="slider">
+                <div className="slider" onTouchMove={this.touchMove} onTouchStart={this.touchStart} onTouchEnd={this.touchEnd}>
                 <div className="slider__back-button"></div>
-                <div className="slider__wrapper" style={{
-                    'transform': `translateX(-${property.id*(100/properties.length)}%)`
+                <div ref={this.slider} className="slider__wrapper" style={{
+                    'transform': `translateX(${slide}%)`
                 }}>
                 {properties.map((property1)=><Tarjeta key={property1.id} property={property1} activa={property}/>)}
                 </div>
